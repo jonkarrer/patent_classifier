86950327fa95b396c2048c546df245b89635aa69
jkarrer <jkarrer@protect247.com>
2024-08-09 16:26:27 -0400

    wip: configure model

--- src/model.rs
+++ src/model.rs
@@ -1,7 +1,23 @@
-use burn::nn::transformer::TransformerEncoder;
+use burn::{
+    nn::transformer::{TransformerEncoder, TransformerEncoderConfig, TransformerEncoderInput},
+    tensor::{Device, Tensor},
+};
 
 use crate::config::MyBackend;
 
 pub struct Model {
     transformer: TransformerEncoder<MyBackend>,
+}
+
+impl Model {
+    pub fn new(device: &Device<MyBackend>) -> Self {
+        let config = TransformerEncoderConfig::new(512, 2048, 8, 4);
+        Self {
+            transformer: config.init(device),
+        }
+    }
+
+    pub fn forward(&self, features: TransformerEncoderInput<MyBackend>) -> Tensor<MyBackend, 3> {
+        self.transformer.forward(features)
+    }
 }
